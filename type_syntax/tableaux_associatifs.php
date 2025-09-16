c64944805d3d7e55668e853c52e21f4eac7dd76d
fadna <fadna.lakhouchen@gmail.com>
2025-09-16 12:14:12 +0100

    Réorganisation des fichiers: déplacer fichiers dans type_syntax et Fonctions_tableaux

---
+++ type_syntax/tableaux_associatifs.php
@@ -0,0 +1,10 @@
+<?php
+$article = [
+  'title'     => 'Intro Laravel',
+  'excerpt'   => null,
+  'views'     => 120,
+  'published' => true,
+];
+
+$article['author'] = 'Amina';                 // ajout
+$hasViews          = array_key_exists('views', $article); // true même si null
